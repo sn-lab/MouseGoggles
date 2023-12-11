174fc02281b2f6768a265a12adc4c80e0675ea97
Matt Isaacson <misaacson01@gmail.com>
2023-12-11 14:35:33 -0500

    Deleted fbcp-st7789 driver folder (obsolete)

--- fbcp-st7789/kernel/start_kernel_module.sh
+++
@@ -1,3 +0,0 @@
-echo Starting kernel module bcm2835_spi_display.ko
-sudo insmod bcm2835_spi_display.ko
-
--- fbcp-st7789/mem_alloc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <sys/types.h>
-#include <inttypes.h>
-
-extern uint64_t totalCpuMemoryAllocated;
-
-void *Malloc(size_t bytes, const char *reason);
